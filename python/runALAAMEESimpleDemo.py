aa47ad10e04454a23261b6b001423c8b5d920979
Alex Stivala <alex.d.stivala@gmail.com>
2020-02-15 02:58:42 +0100

    scripts to run example

---
+++ python/runALAAMEESimpleDemo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# File:    run ALAAMEESimpleDemo.py
+# Author:  Alex Stivala
+# Created: Februrary 2020
+#
+"""Run the simple demonstration implementation of the Equilibrium
+ Expectation algorithm for estimation of Autologistic Actor Attribute
+ Model (ALAAM) parameters.
+"""
+
+import  ALAAMEESimpleDemo
+
+ALAAMEESimpleDemo.run_example()
+
+
+
