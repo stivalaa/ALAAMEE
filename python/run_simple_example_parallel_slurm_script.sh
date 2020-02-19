8310ad82c51911355d6681b1811d3b776fd22444
Alex Stivala <alex.d.stivala@gmail.com>
2020-02-19 05:03:04 +0100

    script to plot results (copy from EstimNetDirected)

--- python/run_simple_example_parallel_slurm_script.sh
+++ python/run_simple_example_parallel_slurm_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name="ALAAMEE_parallel"
+#SBATCH --job-name="ALAAMEE_parallel_sim"
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=20
 #SBATCH --time=0-01:00:00
+#SBATCH --output=alaamee_simexample-%j.out
+#SBATCH --error=alaamee_simexample-%j.err
 
 
 # Remember must 'module load R/3.2.5' to get Microsoft R Open 3.2.5 
@@ -19,6 +21,11 @@ echo -n "started at: "; date
 CPUS_MINUS_ONE=`expr ${SLURM_CPUS_PER_TASK} - 1`
 
 seq 0 ${CPUS_MINUS_ONE} | parallel -j ${SLURM_CPUS_PER_TASK} --progress --joblog parallel.log ./runALAAMEESimpleDemoParallel.py
+
+
+Rscript ../R/computeALAMEEcovariance.R theta_values_n500_kstar_simulate12750000 dzA_values_n500_kstar_simulate12750000
+
+Rscript ../R/plotALAAMEEresults theta_values_n500_kstar_simulate12750000 dzA_values_n500_kstar_simulate12750000
 
 times
 echo -n "ended at: "; date
