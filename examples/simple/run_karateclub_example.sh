f67c7cccbc2fa69040b789fd89107fc97feb6f3e
Alex Stivala <alex.d.stivala@gmail.com>
2020-02-20 02:11:58 +0100

    change paths for new structureZZ

--- examples/simple/run_karateclub_example.sh
+++ examples/simple/run_karateclub_example.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-time python ./runALAAMEEkarateClub.py
+time python ../../python/runALAAMEEkarateClub.py
 
-Rscript ./plotALAAMEESimpleDemoResults.R  theta_values_karate.txt  dzA_values_karate.txt
+Rscript ../../R/plotALAAMEESimpleDemoResults.R  theta_values_karate.txt  dzA_values_karate.txt
 
