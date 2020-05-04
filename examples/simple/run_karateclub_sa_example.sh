7b57c4fa4db088c97224f688c5e500586dcdfb09
Alex Stivala <alex.d.stivala@gmail.com>
2020-05-04 11:45:55 +0200

    make slurm script

--- examples/simple/run_karateclub_sa_example.sh
+++ examples/simple/run_karateclub_sa_example.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+#SBATCH --job-name="stochastic_approx_alaam"
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=0-21:00:00
+#SBATCH --output=alaam_sa_karate-%j.out
+#SBATCH --error=alaam_sa_karate-%j.err
+
 time python ../../python/runALAAMSAkarateClub.py
 
