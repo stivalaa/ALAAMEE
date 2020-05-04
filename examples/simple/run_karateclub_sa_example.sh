af41c13310696e2370689139282af796929f4bcf
Alex Stivala <alex.d.stivala@gmail.com>
2020-05-04 11:48:51 +0200

    increase memory limit

--- examples/simple/run_karateclub_sa_example.sh
+++ examples/simple/run_karateclub_sa_example.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="stochastic_approx_alaam"
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=2G
 #SBATCH --time=0-21:00:00
 #SBATCH --output=alaam_sa_karate-%j.out
 #SBATCH --error=alaam_sa_karate-%j.err
