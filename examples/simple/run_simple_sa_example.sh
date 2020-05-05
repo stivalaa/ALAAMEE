7ce0a895c8646a55a57175be0986dfb44ec6739e
Alex Stivala <alex.d.stivala@gmail.com>
2020-05-05 09:50:53 +0200

    slurm parmeters for SA simple example

--- examples/simple/run_simple_sa_example.sh
+++ examples/simple/run_simple_sa_example.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#SBATCH --job-name="simstochastic_approx_alaam"
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=0-48:00:00
+#SBATCH --output=alaam_sa_simn500-%j.out
+#SBATCH --error=alaam_sa_simn500-%j.err
+
+export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
 
 time python ../../python/runALAAMSASimpleDemo.py
 
