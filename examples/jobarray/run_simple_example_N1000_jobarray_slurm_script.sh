43778b3af2a0ba1b525e0383d8f940c1234302e8
Alex Stivala <alex.d.stivala@gmail.com>
2020-05-09 04:44:12 +0200

    scripts to run EE ALAAM on 1000 node example

---
+++ examples/jobarray/run_simple_example_N1000_jobarray_slurm_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# use the 
+# run_submit_simple_example_process_jobarray_slurm_script.sh
+# job to submit this job and then process output with R scripts 
+# on completion
+
+#SBATCH --job-name="ALAAMEE_jobarray_parallel_sim"
+#SBATCH --time=0-01:00:00
+#SBATCH --output=alaamee_simexampleN1000-%A_%a.out
+#SBATCH --error=alaamee_simexampleN1000-%A_%a.err
+#SBATCH --ntasks=1
+#SBATCH --mem=100MB
+#SBATCH --array=0-99
+
+
+
+echo SLURM_ARRAY_TASK_ID = ${SLURM_ARRAY_TASK_ID}
+
+python ../../python/runALAAMEESimpleDemoN1000Parallel.py ${SLURM_ARRAY_TASK_ID}
+
+
+times
+echo -n "ended at: "; date
---
+++ python/runALAAMEESimpleDemoN1000Parallel.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#
+# File:    run ALAAMEESimpleDemoN1000Parallel.py
+# Author:  Alex Stivala
+# Created: Februrary 2020
+#
+"""Run the simple demonstration implementation of the Equilibrium
+ Expectation algorithm for estimation of Autologistic Actor Attribute
+ Model (ALAAM) parameters. This version takes run number parameter so that
+ parallel runs can be run with GNU parallel. 
+ Usage:
+     runALAAMEESimpleDemoParallel.py runNumber
+
+  E.g. for 16 parallel runs:
+
+  seq 0 15 |  parallel -j 16 --progress --joblog parallel.log run ALAAMEESimpleDemoParallel.py
+
+
+ Citation for GNU parallel:
+
+  O. Tange (2018): GNU Parallel 2018, Mar 2018, ISBN 9781387509881,
+  DOI https://doi.org/10.5281/zenodo.11460
+
+"""
+import getopt
+import sys
+
+import  ALAAMEESimpleDemo
+from changeStatisticsALAAM import *
+
+
+def usage(progname):
+    """
+    print usage msg and exit
+    """
+    sys.stderr.write("usage: " + progname + " runNumber\n")
+    sys.exit(1)
+
+
+def main():
+    """
+    See usage message in module header block
+    """
+    directed = False
+    try:
+        opts,args = getopt.getopt(sys.argv[1:], "")
+    except:
+        usage(sys.argv[0])
+    for opt,arg in opts:
+        usage(sys.argv[0])
+
+    if len(args) != 1:
+        usage(sys.argv[0])
+
+    runNumber = int(args[0])
+
+    ALAAMEESimpleDemo. run_on_network_attr(
+        '../data/simulated_n1000_bin_cont/n1000_kstar_simulate12750000.txt',
+        [changeDensity, changeActivity, changeContagion, changeoOb, changeoOc],
+        ["Density", "Activity", "Contagion", "Binary", "Continuous"],
+        '../data/simulated_n1000_bin_cont/sample-n1000_bin_cont3800000.txt',
+        '../data/simulated_n1000_bin_cont/binaryAttribute_50_50_n1000.txt',
+        '../data/simulated_n1000_bin_cont/continuousAttributes_n1000.txt',
+        runNumber
+    )
+
+
+
+if __name__ == "__main__":
+    main()
+
+
