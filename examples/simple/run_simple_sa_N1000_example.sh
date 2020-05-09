73fe67143263840e123ee417e058e36536ff3300
Alex Stivala <alex.d.stivala@gmail.com>
2020-05-09 04:32:30 +0200

    example 1000 node network stochastic approx scripts

---
+++ examples/simple/run_simple_sa_N1000_example.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#SBATCH --job-name="simstochastic_approx_alaam"
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=0-12:00:00
+#SBATCH --output=alaam_sa_simn1000-%j.out
+#SBATCH --error=alaam_sa_simn1000-%j.err
+
+export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
+
+time python ../../python/runALAAMSASimpleDemoN1000.py
+
+
---
+++ python/runALAAMSASimpleDemoN1000.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# File:    run ALAAMSASimpleDemoN1000.py
+# Author:  Alex Stivala
+# Created: May 2020
+#
+"""Run the simple demonstration implementation of the Stochastic
+ Approximation algorithm for estimation of Autologistic Actor Attribute
+ Model (ALAAM) parameters with simualted 1000 node network.
+"""
+
+import  ALAAMSASimpleDemo
+
+from changeStatisticsALAAM import *
+
+ALAAMSASimpleDemo. run_on_network_attr(
+        '../data/simulated_n1000_bin_cont/n1000_kstar_simulate12750000.txt',
+        [changeDensity, changeActivity, changeContagion, changeoOb, changeoOc],
+        ["Density", "Activity", "Contagion", "Binary", "Continuous"],
+        '../data/simulated_n1000_bin_cont/sample-n1000_bin_cont3800000.txt',
+        '../data/simulated_n1000_bin_cont/binaryAttribute_50_50_n1000.txt',
+        '../data/simulated_n1000_bin_cont/continuousAttributes_n1000.txt'
+    )
