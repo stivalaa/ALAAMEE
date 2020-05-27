85beff37708a142bbbdecdf836ca95af442ac7ac
Alex Stivala <alex.d.stivala@gmail.com>
2020-05-27 14:39:43 +1000

    remove duplicate output on convergence

--- python/ALAAMSASimpleDemo.py
+++ python/ALAAMSASimpleDemo.py
@@ -117,13 +117,6 @@ def run_on_network_attr(edgelist_filename, param_func_list, labels,
         converged = np.all(np.abs(t_ratio) < 0.1)
         i += 1
 
-        if converged:
-            print 'Converged.'
-            significant = np.abs(theta) > 2 * std_error
-            sys.stdout.write(20*' ' + '  Parameter Std.Error t-ratio\n')
-            for j in xrange(len(theta)):
-                sys.stdout.write('%20.20s % 6.3f    % 6.3f    % 6.3f %c\n' % (labels[j], theta[j], std_error[j], t_ratio[j], ('*' if significant[j] else ' ')))
-        print
 
     if converged:
         print 'Converged.'
