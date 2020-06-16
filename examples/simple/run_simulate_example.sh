bd3ff20bf881a51f70c23b492db2e427098092dc
Alex Stivala <alex.d.stivala@gmail.com>
2020-06-16 12:12:20 +1000

    set PYTHONUNBUFFERED to read output as it is produced

--- examples/simple/run_simulate_example.sh
+++ examples/simple/run_simulate_example.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
+
 SIMSTATS_FILE=stats_sim_n500.txt
 
 time ../../python/runALAAMsimulateSimpleDemo.py  | tee ${SIMSTATS_FILE}
--- examples/simple/run_simulate_example_zoo_sampler.sh
+++ examples/simple/run_simulate_example_zoo_sampler.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
+
 SIMSTATS_FILE=stats_sim_n500_zoo.txt
 
 time ../../python/runALAAMsimulateSimpleDemoZooSampler.py  | tee ${SIMSTATS_FILE}
