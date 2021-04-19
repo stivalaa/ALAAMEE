50156333fa3959f7bf2d255c7e25dd11812b5a9a
Alex Stivala <alex.d.stivala@gmail.com>
2021-04-19 17:38:10 +1000

    change to 10 seeds from 3 as 3 has around 50% chance of not converging should make it a paramter instead of having to change in runALAAMSASimpleDemoSnowball.py

--- python/runALAAMSASimpleDemoSnowball.py
+++ python/runALAAMSASimpleDemoSnowball.py
@@ -15,14 +15,14 @@ from changeStatisticsALAAM import *
 import  ALAAMSASimpleDemo
 
 ALAAMSASimpleDemo.run_on_network_attr(
-        'n500_kstar_simulate12750000_waves2_seeds3_num6700000.txt',
+        'n500_kstar_simulate12750000_waves2_seeds10_num6700000.txt',
         [changeDensity, changeActivity, changeContagion, partial(changeoOb, "binaryAttribute"), partial(changeoOc, "continuousAttribute")],
         ["Density", "Activity", "Contagion", "Binary", "Continuous"],
-        'sample-n500_bin_cont6700000_waves2_seeds3.txt',
-        'binaryAttribute_50_50_n500_waves2_seeds3_num6700000.txt',
-        'continuousAttributes_n500_waves2_seeds3_num6700000.txt',
+        'sample-n500_bin_cont6700000_waves2_seeds10.txt',
+        'binaryAttribute_50_50_n500_waves2_seeds10_num6700000.txt',
+        'continuousAttributes_n500_waves2_seeds10_num6700000.txt',
         catattr_filename = None,
         sampler_func = conditionalALAAMsampler,
-        zone_filename = 'snowball_zonefile_waves2_seeds3_num6700000.txt')
+        zone_filename = 'snowball_zonefile_waves2_seeds10_num6700000.txt')
 
 
