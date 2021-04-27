e33fb5f69703599a978b4e8f3c83317a3362f1dd
Alex Stivala <alex.d.stivala@gmail.com>
2021-04-27 06:08:11 +0200

    modules for ICS cluster. reduce runs to 40

--- examples/parallel/run_karateclub_parallel_slurm_script.sh
+++ examples/parallel/run_karateclub_parallel_slurm_script.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="ALAAMEE_parallel_karate"
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=20
-#SBATCH --time=0-10:00:00
+#SBATCH --time=0-4:00:00
 #SBATCH --output=alaamee_karate-%j.out
 #SBATCH --error=alaamee_karate-%j.err
 
@@ -12,14 +12,18 @@ uname -a
 echo SLURM_CPUS_PER_TASK = $SLURM_CPUS_PER_TASK
 echo -n "started at: "; date
 
-NUM_RUNS=100
+module load python/3.8.5
+
+NUM_RUNS=40
 NUM_RUNS_MINUS_ONE=`expr ${NUM_RUNS} - 1`
 
 # use default (no --jobs option) jobs in parallel (one per CPU default)
 seq 0 ${NUM_RUNS_MINUS_ONE} | parallel  --progress --joblog parallel.log ../../python/runALAAMEEkarateClubParallel.py
 
 
+module unload python # on cluster module load r will not work if this is not done
 module load r
+
 
 Rscript ../../R/plotALAAMEEResults.R theta_values_karate dzA_values_karate
 
