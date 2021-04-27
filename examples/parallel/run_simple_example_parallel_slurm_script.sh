3b29ab6793097fcb962322d94ff10294ce3f17f8
Alex Stivala <alex.d.stivala@gmail.com>
2021-04-27 02:47:58 +0200

    module load and unload for ICS cluster. Reduce runs to 40

--- examples/parallel/run_simple_example_parallel_slurm_script.sh
+++ examples/parallel/run_simple_example_parallel_slurm_script.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="ALAAMEE_parallel_sim"
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=20
-#SBATCH --time=0-05:00:00
+#SBATCH --time=0-02:00:00
 #SBATCH --output=alaamee_simexample-%j.out
 #SBATCH --error=alaamee_simexample-%j.err
 
@@ -13,7 +13,9 @@ uname -a
 echo SLURM_CPUS_PER_TASK = $SLURM_CPUS_PER_TASK
 echo -n "started at: "; date
 
-NUM_RUNS=100
+module load python/3.8.5
+
+NUM_RUNS=40
 
 echo NUM_RUNS = $NUM_RUNS
 NUM_RUNS_MINUS_ONE=`expr ${NUM_RUNS} - 1`
@@ -22,6 +24,7 @@ NUM_RUNS_MINUS_ONE=`expr ${NUM_RUNS} - 1`
 seq 0 ${NUM_RUNS_MINUS_ONE} | parallel --progress --joblog parallel.log ../../python/runALAAMEESimpleDemoParallel.py
 
 
+module unload python # on cluster module load r will not work if this is not done
 module load r
 
 Rscript ../../R/plotALAAMEEResults.R theta_values_n500_kstar_simulate12750000 dzA_values_n500_kstar_simulate12750000
