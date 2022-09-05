377a5915d953eb5524179f7db921c22d6fa094be
Alex Stivala <alex.d.stivala@gmail.com>
2022-09-06 09:51:27 +1000

    added undirected karate change stats

--- tests/tests.py
+++ tests/tests.py
@@ -9,9 +9,9 @@
 """
 import time
 from functools import partial
-
+import numpy
 from Graph import Graph
-
+from computeObservedStatistics import computeObservedStatistics
 from changeStatisticsALAAM import *
 
 def test_undirected_change_stats_karate():
@@ -29,7 +29,14 @@ def test_undirected_change_stats_karate():
     assert round(g.density(), 7) == 0.1390374 # from R/igraph
     print(time.time() - start, "s")
     g.printSummary()
-    
+    outcome_binvar = list(map(int, open("../examples/data/karate_club/karate_outcome.txt").read().split()[1:]))
+    obs_stats = computeObservedStatistics(g, outcome_binvar, [changeDensity, changeActivity, changeTwoStar, changeThreeStar, changeContagion, changeTriangleT1, changeTriangleT2, changeTriangleT3, changeIndirectPartnerAttribute, changePartnerAttributeActivity, changePartnerPartnerAttribute, partial(changeoOb, "senior"), partial(changeo_Ob, "senior"), partial(changeoOc, "age"), partial(changeo_Oc, "age"), partial(changeoO_Osame, "gender")])
+    print(obs_stats)
+    assert all(numpy.round(obs_stats, 3) == numpy.array([18, 80, 271, 955, 35, 57, 55, 18, 219, 477, 416, 12, 49, 578.407, 2537.189, 20])) # verified on IPNet 1.5  (nb IPNet 1.5 uses binattr not contattr on matching hence 20 not 40 for oO_Osame_gender)
+    obs_alter2star1 = computeObservedStatistics(g, outcome_binvar,
+                                                [changePartnerActivityTwoPath])
+    print (obs_alter2star1)
+    assert obs_alter2star1 == [566] # verified on MPNet
 
 def main():
     """main: run all tests
