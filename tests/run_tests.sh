f9d8a7da03183070b6333007d4f14602956be7c8
Alex Stivala <alex.d.stivala@gmail.com>
2022-09-06 09:57:11 +1000

    comment unbuffered python var

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,7 +3,7 @@
 command -v module >/dev/null 2>&1 && module load python/3.9.0
 
 export PYTHONPATH=../python:${PYTHONPATH}
-export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
+#export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
 
 time python3 ./tests.py
 
