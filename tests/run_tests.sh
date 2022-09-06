91a62098bfaa8004cd8f394194df5c7342bffea4
Alex Stivala <alex.d.stivala@gmail.com>
2022-09-06 10:26:24 +1000

    more output in tests sh script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+echo -n "started at: "; date
+uname -a
 command -v module >/dev/null 2>&1 && module load python/3.9.0
 
 export PYTHONPATH=../python:${PYTHONPATH}
 #export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
 
 time python3 ./tests.py
+echo -n "ended at: "; date
 
 
