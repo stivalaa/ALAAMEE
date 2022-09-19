829f6db43a31bff26f8050b2193d23a74bb17024
Alex Stivala <alex.d.stivala@gmail.com>
2022-09-19 07:12:56 +0200

    add module load for ICS cluster

--- examples/simple/run_sa_bipartite_example.sh
+++ examples/simple/run_sa_bipartite_example.sh
@@ -8,8 +8,15 @@
 #SBATCH --output=alaam_sa_inouye-%j.out
 #SBATCH --error=alaam_sa_inouye-%j.err
 
+echo -n "started at: "; date
+uname -a
+
+module purge # otherwise sometimes module load fails
+module load python/3.9.0
+
 export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
 
 time python3 ../../python/runALAAMSABipartiteSimpleDemo.py
 
 
+echo -n "ended at: "; date
