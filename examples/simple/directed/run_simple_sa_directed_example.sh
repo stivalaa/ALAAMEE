576e1aad3b2d0d459d89f823bf18e5b5b97a3934
Alex Stivala <alex.d.stivala@gmail.com>
2023-05-04 03:51:44 +0200

    module load python 39

--- examples/simple/directed/run_simple_sa_directed_example.sh
+++ examples/simple/directed/run_simple_sa_directed_example.sh
@@ -8,6 +8,8 @@
 #SBATCH --output=alaam_sa_directed-%j.out
 #SBATCH --error=alaam_sa_directed-%j.err
 
+command -v module >/dev/null 2>&1 && module load python/3.9.0
+
 export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
 
 time python3 ../../../python/runALAAMSASimpleDemoDirected.py
