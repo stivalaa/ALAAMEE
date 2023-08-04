80e7f4ae815667f90a863e2149e0149195189140
Alex Stivala <alex.d.stivala@gmail.com>
2023-08-04 15:24:27 +1000

    implemented get_observed_stats_from_network_attr

--- python/computeObservedStatistics.py
+++ python/computeObservedStatistics.py
@@ -10,6 +10,7 @@ statistics for each 1 variable in the outcome variable vector.
 import numpy as np         # used for matrix & vector data types and functions
 
 from Graph import Graph,NA_VALUE
+from util import int_or_na
 from changeStatisticsALAAM import *
 
 
@@ -41,3 +42,71 @@ def computeObservedStatistics(G, Aobs, changestats_func_list):
             Acopy[i] = 1
     assert(np.all(Acopy == Aobs))
     return Zobs
+
+
+
+def get_observed_stats_from_network_attr(edgelist_filename, param_func_list,
+                                         labels,
+                                         outcome_bin_filename,
+                                         binattr_filename=None,
+                                         contattr_filename=None,
+                                         catattr_filename=None,
+                                         directed=False,
+                                         bipartite=False):
+    """Compute observed stats for outcome on specified network with binary
+    and/or continuous and categorical attributes.
+
+    Parameters:
+         edgelist_filename - filename of Pajek format edgelist
+         param_func_list   - list of change statistic functions corresponding
+                             to statistics to compute
+         labels            - list of strings corresponding to param_func_list
+                             to label output (header line)
+         outcome_bin_filename - filename of binary attribute (node per line)
+                                of outcome variable for ALAAM
+         binattr_filename - filename of binary attributes (node per line)
+                            Default None, in which case no binary attr.
+         contattr_filename - filename of continuous attributes (node per line)
+                            Default None, in which case no continuous attr.
+         catattr_filename - filename of continuous attributes (node per line)
+                            Default None, in which case no categorical attr.
+         directed        - Default False.
+                           True for directed network else undirected.
+         bipartite       - Default False.
+                           True for two-mode network else one-mode.
+
+    Write output to stdout in format readable by R script
+    plotSimulationDiagnostics.R
+
+    """
+    assert(len(param_func_list) == len(labels))
+
+    G = Digraph(edgelist_filename, binattr_filename, contattr_filename,
+               catattr_filename)
+
+    if directed:
+        if bipartite:
+            raise Exception("directed bipartite network not suppored")
+        G = Digraph(edgelist_filename, binattr_filename, contattr_filename,
+                    catattr_filename, zone_filename)
+    else:
+        if bipartite:
+            G = BipartiteGraph(edgelist_filename, binattr_filename,
+                               contattr_filename, catattr_filename,
+                               zone_filename)
+        else:
+            G = Graph(edgelist_filename, binattr_filename,
+                      contattr_filename, catattr_filename, zone_filename)
+
+
+    outcome_binvar = list(map(int_or_na, open(outcome_bin_filename).read().split()[1:]))
+    assert(len(outcome_binvar) == G.numNodes())
+    A = outcome_binvar
+
+    assert( all([x in [0,1,NA_VALUE] for x in A]) )
+
+    # Calculate observed statistics by summing change stats for each 1 variable
+    Zobs = computeObservedStatistics(G, A, param_func_list)
+
+    sys.stdout.write(' '.join(labels) + '\n')
+    sys.stdout.write(' '.join([str(z) for z in Zobs]) + '\n')
