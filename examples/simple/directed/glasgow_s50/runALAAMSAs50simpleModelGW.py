88b7e54bcf0d66d05b98ab2d5c82886cb2bb93ca
Alex Stivala <alex.d.stivala@gmail.com>
2023-08-22 15:27:20 +1000

    simple example with GWSender instead of Sender

---
+++ examples/simple/directed/glasgow_s50/runALAAMSAs50simpleModelGW.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#
+# File:    runALAAMSAs50simpleModelGW.py
+# Author:  Alex Stivala
+# Created: August 2023
+#
+"""Run the python implementation of the Stochastic
+ Approximation algorithm for estimation of Autologistic Actor Attribute
+ Model (ALAAM) parameters on the directed network
+ excerpt of 50 girls from 'Teenage Friends and Lifestyle Study' data
+ from SIENA.
+
+ See README file and downloadAndConvertSIENAs50DataToALAAMEEformat.R
+ in this directory for more details.
+
+ This version is a simple model similar to that shown in
+ https://github.com/johankoskinen/ALAAM/blob/main/ALAAM%20tutorial.Rmd
+ but with Geometrically Weighted Sender rather than Sender.
+ 
+"""
+from functools import partial
+from math import log
+import  estimateALAAMSA
+from changeStatisticsALAAMdirected import *
+from changeStatisticsALAAM import changeDensity, changeoOc
+
+estimateALAAMSA.run_on_network_attr(
+        's50-friendships-directed.net',
+        [changeDensity, changeContagion, changeContagionReciprocity, partial(changeoOc, "sport"), partial(changeoOc, "alcohol"), partial(changeGWSender, log(2.0)), changeReciprocity],
+        ["Density",     "Contagion",     "ContagionReciprocity",     "sport_oOc",                 "alcohol_oOc",                 "GWSender",     "Reciprocity"],
+        outcome_bin_filename = 's50-outcome.txt',
+        binattr_filename = 's50-binattr.txt',
+        contattr_filename = 's50-contattr.txt',
+        catattr_filename = 's50-catattr.txt',
+        directed = True
+    )
---
+++ examples/simple/directed/glasgow_s50/run_sa_s50_example_simple_model_gw.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#SBATCH --job-name="stochastic_approx_alaam_s50_simple_model_gw"
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=50MB
+#SBATCH --time=0-00:10:00
+#SBATCH --output=alaam_sa_s50_simple_model_gw-%j.out
+#SBATCH --error=alaam_sa_s50_simple_model_gw-%j.err
+
+echo -n "started at: "; date
+uname -a
+
+command -v module >/dev/null 2>&1 && module load python/3.9.0
+
+export PYTHONPATH=../../../../python:${PYTHONPATH}
+export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
+
+time python3 runALAAMSAs50simpleModelGW.py
+
+echo -n "ended at: "; date
