62291bf653278c3c684c03bc2bd80bb9cfaafc51
Alex Stivala <alex.d.stivala@gmail.com>
2023-08-24 10:38:09 +1000

    simpler model add model for class bio3 outcome

--- examples/simple/directed/HighSchoolFriendship/runALAAMSAhighschool_class.py
+++ examples/simple/directed/HighSchoolFriendship/runALAAMSAhighschool_class.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 #
-# File:    runALAAMSAhighschool_gender.py
+# File:    runALAAMSAhighschool_class.py
 # Author:  Alex Stivala
 # Created: May 2023
 #
 """Run the python implementation of the Stochastic
  Approximation algorithm for estimation of Autologistic Actor Attribute
  Model (ALAAM) parameters on the SocioPatterns high school frienship network
- with gender as "outcome" variable.
+ with class as "outcome" variable.
 
  See ../../../data/directed/HighSchoolFriendship/README.txt 
  for more details.
@@ -23,7 +23,8 @@ estimateALAAMSA.run_on_network_attr(
         '../../../data/directed/HighSchoolFriendship/highschool_friendship_arclist.net',
         param_func_list,
         [param_func_to_label(f) for f in param_func_list],
-        outcome_bin_filename = '../../../data/directed/HighSchoolFriendship/highschool_friendship_binattr.txt', # 1 means male
+        outcome_bin_filename = '../../../data/directed/HighSchoolFriendship/highschool_friendship_class2BIO3.txt', # 1 means class 2BIO3
+        binattr_filename = '../../../data/directed/HighSchoolFriendship/highschool_friendship_binattr.txt',
         catattr_filename = '../../../data/directed/HighSchoolFriendship/highschool_friendship_catattr.txt',
         directed = True
     )
