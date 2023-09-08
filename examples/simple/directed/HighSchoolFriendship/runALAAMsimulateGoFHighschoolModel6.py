17bc09512102414886710b34de508f6cf94dde62
Alex Stivala <alex.d.stivala@gmail.com>
2023-09-08 12:12:43 +1000

    script to do gof sim plots for Model 6 in manuscript

---
+++ examples/simple/directed/HighSchoolFriendship/runALAAMsimulateGoFHighschoolModel6.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#
+# File:    run runALAAMsimulateGoFHighschoolModel6.py
+# Author:  Alex Stivala
+# Created: September 2023
+#
+"""Simulate from  Autologistic Actor Attribute Model (ALAAM).
+   
+   Usage: runALAAMsimulateGoFHighschoolModel6.py
+
+"""
+import sys
+from functools import partial
+import numpy as np
+from math import log
+
+from changeStatisticsALAAM import changeDensity,param_func_to_label
+from changeStatisticsALAAMdirected import *
+from simulateALAAM import simulate_from_network_attr
+from parseEstimationEEOutput import parseEstimationEEOutput
+
+
+###
+### main
+###
+
+        
+param_func_list =  [changeDensity, partial(changeGWSender, log(2.0)), partial(changeGWReceiver, log(2.0)), changeContagion, changeReciprocity, changeContagionReciprocity, changeMixedTwoStarSource, changeMixedTwoStarSink, changeTransitiveTriangleT1, changeTransitiveTriangleT3, partial(changeSenderMatch, "class"), partial(changeReceiverMatch, "class"), partial(changeReciprocityMatch, "class")]
+
+## theta vaules from alaam_sa_highschool_gender_gw_more.out
+## TODO parse from SA output like parseEstimationEEOutput() does for EE output
+theta     = np.array([-2.63655716,5.25906295,-0.57848754,0.72511577,-0.09219398,-1.04759659,0.02454693,-0.00771666,-0.04788056,-0.01170659,0.10484196,-0.03113242,0.18434274])
+
+labels =  [param_func_to_label(f) for f in param_func_list]
+
+
+## Add extra statistics if not in model for goodness-of-fit
+statfuncs = [changeSender, changeReceiver, changeReciprocity,
+             changeEgoInTwoStar, changeEgoOutTwoStar,
+             changeMixedTwoStar, changeMixedTwoStarSource,
+             changeMixedTwoStarSink, changeContagion,
+             changeContagionReciprocity,
+             changeTransitiveTriangleT1,
+             changeTransitiveTriangleT3,
+             changeTransitiveTriangleD1,
+             changeTransitiveTriangleU1,
+             changeCyclicTriangleC1,
+             changeCyclicTriangleC3,
+             changeAlterInTwoStar2,
+             changeAlterOutTwoStar2]
+param_func_list += [f for f in statfuncs if f not in param_func_list]
+labels =  [param_func_to_label(f) for f in param_func_list]
+
+# pad theta vector with zeros for the parameters not in the model
+theta = np.array(list(theta) + (len(param_func_list)-len(theta))*[0])
+
+
+assert len(param_func_list) == len(labels)
+assert len(theta) == len(param_func_list)
+
+
+simulate_from_network_attr(
+    '../../../data/directed/HighSchoolFriendship/highschool_friendship_arclist.net',
+    param_func_list, labels, theta,
+    iterationInStep = 1000,
+    burnIn = 10000,
+    directed = True,
+    outputSimulatedVectors = True,
+    simvecFilePrefix = "sim_outcome_model6"
+    )
+
+
+
---
+++ examples/simple/directed/HighSchoolFriendship/run_simulate_gof_highschool_model6_slurm_script.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#SBATCH --job-name="gof_ALAAM"
+#SBATCH --time=0-00:10:00
+#SBATCH --output=gof_alaam_sim_highschool_model6-%j.out
+#SBATCH --error=gof_alaam_sim_highschool_model6-%j.err
+#SBATCH --ntasks=1
+#SBATCH --mem=4GB
+
+echo -n "started at: "; date
+uname -a
+
+command -v module >/dev/null 2>&1 && module load foss/2022b
+command -v module >/dev/null 2>&1 && module load python/3.10.8
+command -v module >/dev/null 2>&1 && module load numpy/1.24.2-scipy-bundle-2023.02
+
+command -v module >/dev/null 2>&1 && module load gcc/11.3.0 # needed by r/4.2.1
+command -v module >/dev/null 2>&1 && module load openmpi/4.1.4 # needed by r/4.2.1
+command -v module >/dev/null 2>&1 && module load r/4.2.1
+    
+
+# run simulation from parameters and plot observed
+# statistics of network from which the parmeters were estimated as
+# GoF check
+
+# output file of observed ALAAM statistics
+OBSTATS_FILE=obs_stats_sim_highschool.txt
+
+ROOT=../../../../
+export PYTHONPATH=${ROOT}/python:${PYTHONPATH}
+export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
+
+time python3 ./computeALAAMstatisticsHighschool.py | tee ${OBSTATS_FILE}
+
+modelname=model6
+
+# output file of simulated ALAAM statistics
+SIMSTATS_FILE=stats_sim_gof_highschool_${modelname}.txt
+
+time python3 ./runALAAMsimulateGoFHighschool.py ${modelname}  | tee ${SIMSTATS_FILE}
+
+Rscript ${ROOT}/R/plotSimulationDiagnostics.R  ${SIMSTATS_FILE} ${OBSTATS_FILE}
+
+Rscript ${ROOT}/R/plotALAAMEEsimFit.R -a ../../../data/directed/HighSchoolFriendship/highschool_friendship_arclist.net ../../../data/directed/HighSchoolFriendship/highschool_friendship_binattr.txt sim_outcome_${modelname}
+
+Rscript ${ROOT}/R/plotALAAMEEvsRandomDegreeDist.R  ../../../data/directed/HighSchoolFriendship/highschool_friendship_arclist.net ../../../data/directed/HighSchoolFriendship/highschool_friendship_binattr.txt sim_outcome_${modelname}
+
+times
+echo -n "ended at: "; date
