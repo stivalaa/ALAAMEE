329f37f728d89599084a6a64c40abbf8a72fb4c9
Alex Stivala <alex.d.stivala@gmail.com>
2023-09-18 10:32:34 +1000

    remove task array line from gnu parallel script

--- examples/jobarray/GitHub/EE_ALAAMEE/run_github_parallel_slurm_script.sh
+++ examples/jobarray/GitHub/EE_ALAAMEE/run_github_parallel_slurm_script.sh
@@ -21,7 +21,6 @@ NUM_RUNS_MINUS_ONE=`expr ${NUM_RUNS} - 1`
 module load python/3.9.0
 
 export PYTHONPATH=${ROOT}/python/:${PYTHONPATH}
-python3 ./runALAAMEEGithubParallel.py ${SLURM_ARRAY_TASK_ID}
 
 # use default (no --jobs option) jobs in parallel (one per CPU default)
 #seq 0 ${NUM_RUNS_MINUS_ONE} | parallel --progress --joblog parallel.log ./runALAAMEEGithubParallel.py
