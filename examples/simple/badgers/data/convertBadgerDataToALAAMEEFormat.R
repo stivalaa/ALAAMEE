6cc7c491fcd0dc8cf2bcac005be58f53c2687ed6
Alex Stivala <alex.d.stivala@gmail.com>
2023-10-02 15:30:58 +1100

    script to convert badger data seems to be working

--- examples/simple/badgers/data/convertBadgerDataToALAAMEEFormat.R
+++ examples/simple/badgers/data/convertBadgerDataToALAAMEEFormat.R
@@ -49,10 +49,12 @@ Xbinary <- ifelse(X > 0, 1, 0)
 g <- graph_from_adjacency_matrix(Xbinary, mode='undirected',
                                  weighted=NULL, diag=FALSE)
 
+summary(g)
+
 ## log edge weights in original adjacency matrix (used later for centrality)
 X <- ifelse(X == 0, 0, log(X))
 
-## Read bTB status and convert "N" and "P" to 0 and 1
+## Read bTB status and convert "N" and "P" to 0 and 1 respectively
 TB <- read.csv(unz(infile, "TBstatsF.csv"))
 stopifnot(all(TB$TBend %in% c("N", "P")))
 stopifnot(all(TB$Badger == V(g)$name))
@@ -61,11 +63,65 @@ TB$TBstatus <- ifelse(TB$TBend == "P", 1, 0)
 ## Read ages, these are "Yearling" and "Adult"
 Ages <- read.csv(unz(infile, "Ages.csv"))
 stopifnot(all(Ages$ID == V(g)$name))
+stopifnot(all(Ages$Age %in% c("Yearling", "Adult")))
 
 ## Read sexes, these are "M" and "F"
 Sex <- read.csv(unz(infile, "indivsexes.csv"))
 stopifnot(all(Sex$ID == V(g)$name))
+stopifnot(all(Sex$Sex %in% c("M", "F")))
 
 ## Read in social group membership
 groups <- read.csv(unz(infile, "Complete Membership.csv"))
 stopifnot(all(groups$ID == V(g)$name))
+
+## Create network of within-group interactions X2
+X2 <- outer(1:vcount(g), 1:vcount(g), 
+           FUN =  Vectorize(function(i, j) 
+                           as.numeric(groups[i,"Group"] == groups[j, "Group"])))
+
+## Create network of between-group interactions X3
+X3 <- outer(1:vcount(g), 1:vcount(g), 
+           FUN =  Vectorize(function(i, j) 
+                           as.numeric(groups[i,"Group"] != groups[j, "Group"])))
+
+
+## Write network
+write.graph(g, file="badgers_overallnetwork.net", format="pajek")
+
+## Write outcome binary attribute (bTB positive)
+outcomebinattr <- data.frame(TBpos = TB$TBstatus)
+summary(outcomebinattr)
+write.table(outcomebinattr, file = "badgers_TBpos.txt",
+            row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+## Write binary attributes
+binattr <- data.frame(male = ifelse(Sex$Sex == "M", 1, 0), # Male is 1
+                      adult = ifelse(Ages$Age == "Adult", 1, 0)) # Adult is 1
+summary(binattr)
+write.table(binattr, file = "badgers_binattr.txt",
+            row.names = FALSE, col.names = TRUE, quote = FALSE)
+                  
+
+## Write categorical attributes
+catattr <- data.frame(group = groups$Group)
+summary(catattr)
+write.table(catattr, file = "badgers_catattr.txt",
+            row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+
+## Compute centralites and write as continuous attributes
+## (note flow betweenness takes a few seconds even on this small network)
+system.time( betweenGroupFlow <- flowbet(X3, gmode = "graph", rescale = TRUE) )
+withinGroupEigen <- eigen_centrality(
+                      graph_from_adjacency_matrix(X2, mode='undirected',
+                                            weighted=TRUE, diag=FALSE))$vector
+
+withinGroupDegree <- sna::degree(X2, gmode="graph", rescale=TRUE)
+contattr <- data.frame(betweenGroupFlowCent = betweenGroupFlow,
+                       withinGroupEigenCent = withinGroupEigen,
+                       withinGroupDegreeCent = withinGroupDegree)
+summary(contattr)
+write.table(contattr, file = "badgers_contattr.txt",
+            row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+
