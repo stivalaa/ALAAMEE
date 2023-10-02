bef77b0b4b47a6fb08d255b857184f20d79b7527
Alex Stivala <alex.d.stivala@gmail.com>
2023-10-02 16:28:37 +1100

    version wit ACtivity and not within-group degree

---
+++ examples/simple/badgers/run_sa_badgers.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#SBATCH --job-name="stochastic_approx_alaam_badgers"
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=40MB
+#SBATCH --time=0-00:10:00
+#SBATCH --output=alaam_sa_badgers-%j.out
+#SBATCH --error=alaam_sa_badgers-%j.err
+
+echo -n "started at: "; date
+uname -a
+
+command -v module >/dev/null 2>&1 && module load python/3.9.0
+
+export PYTHONPATH=../../../python:${PYTHONPATH}
+export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
+
+time python3 runALAAMSAbadgers.py
+
+echo -n "ended at: "; date
