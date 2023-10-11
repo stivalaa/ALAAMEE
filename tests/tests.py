fc66adb433d249731069221fffe3a91eb935a8dc
Alex Stivala <alex.d.stivala@gmail.com>
2023-10-12 09:27:02 +1100

    comment out debug prints

--- tests/tests.py
+++ tests/tests.py
@@ -125,8 +125,8 @@ def compare_statistic_sum_changestatistic(g, outcome_binvar, stat_func,
     change_stat_sum = computeObservedStatistics(g, outcome_binvar,
                                                 [changestats_func])[0]
     stat_value =  stat_func(g, outcome_binvar)
-    print(stat_value)
-    print(change_stat_sum)
+    #print(stat_value)
+    #print(change_stat_sum)
     if epsilon is not None:
         assert abs(change_stat_sum - stat_value) < epsilon
     else:
