f499b9dc10b6e22b77b301cb25f8504b9b788df8
Alex Stivala <alex.d.stivala@gmail.com>
2024-03-06 15:16:28 +1100

    new module left off last commit

---
+++ examples/jobarray/GemsecDeezer/EE_ALAAMEE/modelHR.py
@@ -0,0 +1,23 @@
+"""
+Defines the model in terms of the list of parameters to estimate
+(also used for computing observed statistics and simulation for GoF)
+"""
+from functools import partial
+from math import log
+from changeStatisticsALAAM import *
+
+
+param_func_list = [changeDensity, partial(changeGWActivity, 2.0),
+                   changeContagion,
+                   partial(changeoOc, "num_genres"),
+                   partial(changeo_Oc, "num_genres")]
+
+
+## [RO] Gof sim OK on all but IndirectPartnerAttribute 
+## [HR] GoF sim bad (well oustide 95% CI) on all but GWActivity(2) which is good
+#param_func_list = [changeDensity, partial(changeGWActivity, 2.0),
+#                   changeContagion,
+#                   changeTriangleT1, changeTriangleT2, changeTriangleT3,
+#                   partial(changeoOc, "num_genres"),
+#                   partial(changeo_Oc, "num_genres")]
+
