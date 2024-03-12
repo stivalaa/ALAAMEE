cc0c8ec165f91079163c932815cf1c5dfd1ed5a9
Alex Stivala <alex.d.stivala@gmail.com>
2024-03-13 09:28:13 +1100

    scripts for stochastic approx with alternative outcome

---
+++ examples/jobarray/GemsecDeezer/SA_ALAAMEE_alternative/runALAAMSAgemsecDeezerRO.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#
+# File:    run ALAAMSAdeezer.py
+# Author:  Alex Stivala
+# Created: May 2023
+#
+"""Run the python implementation of the Stochastic
+ Approximation algorithm for estimation of Autologistic Actor Attribute
+ Model (ALAAM) parameters on the Deezer Europe network
+"""
+from functools import partial
+
+import  estimateALAAMSA
+from changeStatisticsALAAM import *
+
+param_func_list = [changeDensity, partial(changeGWActivity, 2.0),
+                   changeContagion,
+                   changeTriangleT1, changeTriangleT2, changeTriangleT3,
+                   partial(changeoOc, "num_genres"),
+                   partial(changeo_Oc, "num_genres")]
+
+estimateALAAMSA.run_on_network_attr(
+    '../data/deezer_ro_friendship.net',
+    param_func_list,
+    [param_func_to_label(f) for f in param_func_list],
+    '../data/deezer_ro_outcome_alternative.txt',
+    contattr_filename = '../data/deezer_ro_contattr.txt'
+    )
+
+
+
