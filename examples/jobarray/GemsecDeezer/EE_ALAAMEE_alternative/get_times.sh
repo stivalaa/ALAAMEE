a3fcb0782ce1e24bc4686ddd41359ae792fc97af
Alex Stivala <alex.d.stivala@gmail.com>
2024-03-18 12:09:46 +1100

    script ot get job times

---
+++ examples/jobarray/GemsecDeezer/EE_ALAAMEE_alternative/get_times.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# get elapsed time in minutes and make histogram, saving times in minutes to file
+
+#WARNING: overwritten
+
+country=ro
+echo ${country}
+outfile=jobtimes_${country}.txt
+hist_outfile=jobtimes_${country}_histogram.txt
+
+../../../../scripts/getbashtimes.sh  alaamee_GEMSEC_Deezer_RO-49061538_*.out| awk '{print $1 / 60}'  >  ${outfile}
+
+# histogram.py does not work on OzSTAR as it requires python 2 so use 
+# histogram.sh intead to load thie python 2 module
+cat ${outfile} | histogram.sh | tee ${hist_outfile}
+
+country=hr
+echo ${country}
+outfile=jobtimes_${country}.txt
+hist_outfile=jobtimes_${country}_histogram.txt
+
+../../../../scripts/getbashtimes.sh alaamee_GEMSEC_Deezer_HR-49061551_*.out| awk '{print $1 / 60}'  >  ${outfile}
+
+# histogram.py does not work on OzSTAR as it requires python 2 so use 
+# histogram.sh intead to load thie python 2 module
+cat ${outfile} | histogram.sh | tee ${hist_outfile}
+
+country=hu
+echo ${country}
+outfile=jobtimes_${country}.txt
+hist_outfile=jobtimes_${country}_histogram.txt
+
+../../../../scripts/getbashtimes.sh alaamee_GEMSEC_Deezer_HU-49061554_*.out  | awk '{print $1 / 60}'  >  ${outfile}
+
+# histogram.py does not work on OzSTAR as it requires python 2 so use 
+# histogram.sh intead to load thie python 2 module
+cat ${outfile} | histogram.sh | tee ${hist_outfile}
