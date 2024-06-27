0ae34bc39cddc30d0aa4419c78b28dbd98806782
Alex Stivala <alex.d.stivala@gmail.com>
2024-06-27 13:21:01 +1000

    manually verified tests pass

--- tests/tests.py
+++ tests/tests.py
@@ -882,8 +882,8 @@ def test_new_bipartite_change_stats_tiny():
     obs_stats = computeObservedStatistics(g, outcome_binvar, [partial(changeBipartiteAlterMatchingTwoStar1, MODE_A, 'catattr'), partial(changeBipartiteAlterMatchingTwoStar2, MODE_A, 'catattr'), partial(changeBipartiteAlterMismatchingTwoStar1, MODE_A, 'catattr'), partial(changeBipartiteAlterMismatchingTwoStar2, MODE_A, 'catattr'), partial(changeBipartiteAlterBinaryTwoStar1, MODE_A, 'binattr'), partial(changeBipartiteAlterBinaryTwoStar1, MODE_A, 'binattr')])
     assert all(obs_stats == numpy.array([0, 0, 0, 0, 0, 0])) #mode A all zero
 
-    obs_stats = computeObservedStatistics(g, outcome_binvar, [partial(changeBipartiteAlterMatchingTwoStar1, MODE_B, 'catattr'), partial(changeBipartiteAlterMatchingTwoStar2, MODE_B, 'catattr'), partial(changeBipartiteAlterMismatchingTwoStar1, MODE_B, 'catattr'), partial(changeBipartiteAlterMismatchingTwoStar2, MODE_B, 'catattr'), partial(changeBipartiteAlterBinaryTwoStar1, MODE_B, 'binattr'), partial(changeBipartiteAlterBinaryTwoStar1, MODE_B, 'binattr')])
-    assert all(obs_stats == numpy.array([0, 0, 0, 0, 0, 0]))
+    obs_stats = computeObservedStatistics(g, outcome_binvar, [partial(changeBipartiteAlterMatchingTwoStar1, MODE_B, 'catattr'), partial(changeBipartiteAlterMatchingTwoStar2, MODE_B, 'catattr'), partial(changeBipartiteAlterMismatchingTwoStar1, MODE_B, 'catattr'), partial(changeBipartiteAlterMismatchingTwoStar2, MODE_B, 'catattr'), partial(changeBipartiteAlterBinaryTwoStar1, MODE_B, 'binattr'), partial(changeBipartiteAlterBinaryTwoStar2, MODE_B, 'binattr')])
+    assert all(obs_stats == numpy.array([0, 0, 2, 0, 1, 0]))
     print("OK")
     print()
 
