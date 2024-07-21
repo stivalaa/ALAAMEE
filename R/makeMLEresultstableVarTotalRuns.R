bc09e84833971c58e61616ea8140cd27bc8630a6
Alex Stivala <alex.d.stivala@gmail.com>
2024-07-21 11:15:42 +1000

    reformat whitepsace

--- R/makeMLEresultstableVarTotalRuns.R
+++ R/makeMLEresultstableVarTotalRuns.R
@@ -1,22 +1,20 @@
 #!/usr/bin/Rscript
-#
-# File:    makeMLEresultsTableVarTotalRuns.R
-# Author:  Alex Stivala
-# Created: May 2023
-#
-#
-#
-# Read estimation results from
-# build_alaamee_estimation_var_total_runs_results_tab.sh
-# and make table of estimates and standard errors and false negative rates.
-#
-# Usage: Rscript [-s] makeMLEresultsTableVarTotalRuns.R 
-#
-#
-# Output is to stdout
-#
-# e.g.: Rscript makeMLEresultsTableVarTotalRuns.R
-# 
+##
+## File:    makeMLEresultsTableVarTotalRuns.R
+## Author:  Alex Stivala
+## Created: May 2023
+##
+##
+## Read estimation results from
+## build_alaamee_estimation_var_total_runs_results_tab.sh
+## and make table of estimates and standard errors and false negative rates.
+##
+## Usage: Rscript [-s] makeMLEresultsTableVarTotalRuns.R 
+##
+## Output is to stdout
+##
+## e.g.: Rscript makeMLEresultsTableVarTotalRuns.R
+## 
 
 library(PropCIs) # for Wilson score test for false negative rate
 
@@ -38,81 +36,79 @@ if (length(args) > 0 ) {
     }
 }  
 
-# write header line
+## write header line
 cat("N Descr ErrorAnalysis FixedDensity Effect Bias RMSE FNRpercent MeanStdErr StdDevEstimate NumConverged FNRpercentLower FNRpercentUpper TotalNetworksConverged TotalRuns PercentInCI\n")
 
 for (results_filename in results_filenames) {
   Dorig <- read.table(results_filename, header=TRUE, stringsAsFactors=TRUE)
+  
+  ## Project 90 simulated ALAAM parameters as used in Stivala et al (2020)
+  ## arXiv:2002.00849v2 (ALAAM snowball paper) harcoded here
+  
+  effects <- c('Density', 'Activity', 'Contagion', 'binary_oOb', 'continuous_oOc')
 
+  ## output effect names, corresponding to above
+  effect_names <- c('Density', 'Activity', 'Contagion', 'Binary', 'Continuous')
 
-                
-    # Project 90 simulated ALAAM parameters as used in Stivala et al (2020)
-    # arXiv:2002.00849v2 (ALAAM snowball paper) harcoded here
-          
-    effects <- c('Density', 'Activity', 'Contagion', 'binary_oOb', 'continuous_oOc')
+  ## known true values of effects above (for drawing horizontal line on plot)
+  true_parameters <- c(-15.0, 0.55, 1.00, 1.20, 1.15)
 
-    # output effect names, corresponding to above
-    effect_names <- c('Density', 'Activity', 'Contagion', 'Binary', 'Continuous')
-
-    # known true values of effects above (for drawing horizontal line on plot)
-    true_parameters <- c(-15.0, 0.55, 1.00, 1.20, 1.15)
-
-    error_analysis <- 'ALAAMEE'
-    fixeddensity <- 'N'
-    network_N_list <- 4430
-    attribute_descr <- 'Project90simulated'
-    totalRuns_list <- unique(Dorig$totalRuns)
-     
-    for (network_N in network_N_list) {
-     for (this_totalRuns in totalRuns_list) {
+  error_analysis <- 'ALAAMEE'
+  fixeddensity <- 'N'
+  network_N_list <- 4430
+  attribute_descr <- 'Project90simulated'
+  totalRuns_list <- unique(Dorig$totalRuns)
+  
+  for (network_N in network_N_list) {
+    for (this_totalRuns in totalRuns_list) {
       if ('nodeCount' %in% names(Dorig)) {
-          D <- Dorig[which(Dorig$nodeCount == network_N), ]
+        D <- Dorig[which(Dorig$nodeCount == network_N), ]
       }  else {
-          D <- Dorig
+        D <- Dorig
       }
 
       D <- Dorig[which(Dorig$totalRuns == this_totalRuns), ]
 
       if (use_sd_theta) {
-         D$StdErr <- D$sdEstimate # Use sd(theta) as estimated standard eror
+        D$StdErr <- D$sdEstimate ## Use sd(theta) as estimated standard eror
       }
 
-	    if (length(grep('binattr', results_filename)) > 0) {
-	      attribute_descr <- 'Binary' 
-	    }
-	    else if (length(grep('cat3', results_filename)) > 0) {
+      if (length(grep('binattr', results_filename)) > 0) {
+        attribute_descr <- 'Binary' 
+      }
+      else if (length(grep('cat3', results_filename)) > 0) {
         attribute_descr <- 'Categorical'
-	    }
-	    else if (length(grep('cont', results_filename)) > 0) {
+      }
+      else if (length(grep('cont', results_filename)) > 0) {
         attribute_descr <- 'Continuous'
-	    }
+      }
 
 
       this_effects <- effects
-	    this_true_parameters <- true_parameters
+      this_true_parameters <- true_parameters
 
-			for (i in 1:length(this_effects)) {
+      for (i in 1:length(this_effects)) {
         effect <- this_effects[i]
         De <- D[which(D$Effect == effect),]
         if (length(De$Effect) == 0) {
-            next
+          next
         }
+        
 
-
-        # remove unconverged samples 
+        ## remove unconverged samples 
         if ('t.ratio' %in% names(De)) {
-            oldn <- nrow(De)
-            De <- De[which(De$t.ratio <= 0.1), ]
-            if (nrow(De) != oldn) {
-                write(paste('removed',oldn-nrow(De),'unconverged estimates\n'),stderr())
-            }
+          oldn <- nrow(De)
+          De <- De[which(De$t.ratio <= 0.1), ]
+          if (nrow(De) != oldn) {
+            write(paste('removed',oldn-nrow(De),'unconverged estimates\n'),stderr())
+          }
         }
 
-        # remove samples with NaN or infinite Estimate
+        ## remove samples with NaN or infinite Estimate
         De <- De[which(!is.na(De$Estimate)), ]
         De <- De[which(!is.infinite(De$Estimate)),]
-        De <- De[which(abs(De$Estimate) < 1e03),] # sometimes not inf but still huge
-        # remove samples with zero or extreme values as StdErr 
+        De <- De[which(abs(De$Estimate) < 1e03),] ## sometimes not inf but still huge
+        ## remove samples with zero or extreme values as StdErr 
         ## De <- De[which(De$StdErr != 0),]
         ## De <- De[which(!is.na(De$StdErr)),]
         ## De <- De[which(!is.infinite(De$StdErr)),]
@@ -126,19 +122,19 @@ for (results_filename in results_filenames) {
         rmse <- sqrt(mean((De$Estimate - this_true_parameters[i])^2))
         bias <- mean(De$Estimate - this_true_parameters[i])
 
-        # count number of times the CI  includes true value
+        ## count number of times the CI  includes true value
         num_in_ci <- sum((De$Estimate < this_true_parameters[i] & De$Estimate + zSigma*De$StdErr >= this_true_parameters[i]) | (De$Estimate >= this_true_parameters[i] & De$Estimate - zSigma*De$StdErr <= this_true_parameters[i]))
         perc_in_ci <- 100 * num_in_ci / length(De$Estimate)
 
-        # for purely inference (sign and significance, not actual value of estimate)
-        # compute False Negative rate, as the number of times the estimate
-        # is the right sign, but the CI includes zero; or, is the wrong sign.
-            
+        ## for purely inference (sign and significance, not actual value of estimate)
+        ## compute False Negative rate, as the number of times the estimate
+        ## is the right sign, but the CI includes zero; or, is the wrong sign.
+        
         false_negative_count <-
-            sum( (sign(De$Estimate) == sign(this_true_parameters[i]) & 
-                  ((De$Estimate < 0 & De$Estimate + zSigma*De$StdErr >= 0) |
-                    (De$Estimate >= 0 & De$Estimate - zSigma*De$StdErr <= 0)) ) |
-                  sign(De$Estimate) != sign(this_true_parameters[i]) )
+          sum( (sign(De$Estimate) == sign(this_true_parameters[i]) & 
+                ((De$Estimate < 0 & De$Estimate + zSigma*De$StdErr >= 0) |
+                 (De$Estimate >= 0 & De$Estimate - zSigma*De$StdErr <= 0)) ) |
+               sign(De$Estimate) != sign(this_true_parameters[i]) )
         false_negative_perc <- 100 * false_negative_count / length(De$Estimate)
         confint <- scoreci(false_negative_count, length(De$Estimate), 0.95)
         fnp_lower <- confint$conf.int[1] * 100
@@ -149,8 +145,7 @@ for (results_filename in results_filenames) {
             fnp_lower, fnp_upper,
             length(De$Estimate), totalRuns, perc_in_ci)
         cat('\n')
-        }
-
+      }
     }
   }
 }
