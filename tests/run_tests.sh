e25202829b161be80081b7e878f43443cb45b683
Alex Stivala <alex.d.stivala@gmail.com>
2024-08-19 14:51:20 +1000

    tests for igraph conversoin

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,7 +7,14 @@ command -v module >/dev/null 2>&1 && module load python/3.9.0
 export PYTHONPATH=../python:${PYTHONPATH}
 #export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
 
-time python3 ./tests.py
+fail=0
+time python3 ./tests.py || fail=1
+time python3 ./tests_igraph.py || fail=1
+
+if [ $fail -ne 0 ]; then
+    echo "***** A test FAILED *****"
+fi
 echo -n "ended at: "; date
+exit $fail
 
 
---
+++ tests/tests_igraph.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+#
+# File:    tests_igraph.py
+# Author:  Alex Stivala
+# Created: August 2024
+#
+"""Unit and regression tests for ALAAMEE igrph conversion
+(i.e. conversion from igraph to ALAAMEE internal format, particularly
+change statistics computations.
+
+"""
+import sys,os,time,random,gzip
+
+try:
+    import igraph
+except ImportError:
+    print("Could not import igraph, skipping tests.")
+    sys.exit(0)
+
+from igraphConvert import igraphConvert
+
+######################### test functions #####################################
+#
+##############################################################################
+
+def test_undirected_graph():
+
+    """
+    test Graph object from igraph
+    """
+    print("testing Graph object converted from igraph...")
+    start = time.time()
+    g_igraph = igraph.Graph.Read("../examples/data/simulated_n1000_bin_cont/n1000_kstar_simulate12750000.txt", format="pajek")
+    print(g_igraph.summary())
+    g = igraphConvert(g_igraph)
+
+    # specific to this graph
+    assert g.numNodes() == 1000
+    assert g.numEdges() == 3001
+    assert round(g.density(), 9) == round(g_igraph.density(), 9)
+    assert round(g.density(), 9) == 0.006008008 # from R/igraph
+    g.printSummary()
+
+    # following must be true for any Graph
+    assert len(list(g.nodeIterator())) == g.numNodes()
+    assert all([g.isEdge(i, j) for i in g.nodeIterator() for j in g.neighbourIterator(i)])
+    assert all([len(list(g.neighbourIterator(i))) == g.degree(i) for i in g.nodeIterator()])
+    for i in g.nodeIterator():
+        assert(len(list(g.neighbourIterator(i))) == len(set(g.neighbourIterator(i)))) # check no repeated neighbours in iterator
+
+    assert g.numEdges() == len(list(g.edgeIterator()))
+
+    print("OK,", time.time() - start, "s")
+    print()
+
+
+
+
+def test_directed_graph():
+
+    """
+    test Digraph object from igraph
+    """
+    print("testing Diraph object converted from igraph...")
+    start = time.time()
+    datadir = os.path.join("..", "examples", "data", "directed", "HighSchoolFriendship")
+    edgelist_text = gzip.open(os.path.join(datadir,"Friendship-network_data_2013.csv.gz"), mode="rt").readlines()
+    edgelist_tuples = [tuple(s.split()) for s in edgelist_text]
+    g_igraph = igraph.Graph.TupleList(edgelist_tuples, directed = True)
+    
+    print(g_igraph.summary())
+    g = igraphConvert(g_igraph)
+
+    # specific to this graph
+    assert g.numNodes() == 134
+    assert g.numArcs() == 668
+    assert round(g.density(), 9) == round(g_igraph.density(), 9)
+    assert round(g.density(), 9) == 0.037481764 # calculated manually 
+    g.printSummary()
+
+    # following must be true for any Digraph
+    assert len(list(g.nodeIterator())) == g.numNodes()
+    assert all([g.isArc(i, j) for i in g.nodeIterator() for j in g.outIterator(i)])
+    assert all([g.isArc(j, i) for i in g.nodeIterator() for j in g.inIterator(i)])    
+    assert all([len(list(g.outIterator(i))) == g.outdegree(i) for i in g.nodeIterator()]) 
+    assert all([len(list(g.inIterator(i))) == g.indegree(i) for i in g.nodeIterator()])   
+    for i in g.nodeIterator():
+        assert(len(list(g.outIterator(i))) == len(set(g.outIterator(i)))) # check no repeated neighbours in iterator
+        assert(len(list(g.inIterator(i))) == len(set(g.inIterator(i)))) # check no repeated neighbours in iterator        
+
+
+    print("OK,", time.time() - start, "s")
+    print()
+    
+
+
+
+############################### main #########################################
+
+def main():
+    """main: run all tests
+    """
+    test_undirected_graph()
+    test_directed_graph()
+
+
+    
+if __name__ == "__main__":
+    main()
