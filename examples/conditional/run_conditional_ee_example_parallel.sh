4452a6bec51693398e2d4d26cf2f272ec9133fb5
Alex Stivala <alex.d.stivala@gmail.com>
2025-06-26 09:09:47 +1000

    added new formatted text output to example

--- examples/conditional/run_conditional_ee_example_parallel.sh
+++ examples/conditional/run_conditional_ee_example_parallel.sh
@@ -29,7 +29,7 @@ time Rscript ../../R/snowballSampleFromExampleData.R $num_waves $num_seeds ../da
 export PYTHONUNBUFFERED=1    # unbuffered stdout to see progress as it runs
 # use with --keep-order and --line-buffer on GNU parallel
 
-# use defulat (no --jobs option) jobs in parallel (one per CPU default)
+# use default (no --jobs option) jobs in parallel (one per CPU default)
 seq 0 ${NUM_RUNS_MINUS_ONE} | parallel  --progress --joblog parallel.log --keep-order --line-buffer ../../python/runALAAMEESimpleDemoSnowballParallel.py ${num_waves} ${num_seeds}
 
 
@@ -38,6 +38,7 @@ Rscript ../../R/plotALAAMEEResults.R theta_values_n500_kstar_simulate12750000_wa
 
 Rscript ../../R/computeALAMEEcovariance.R  theta_values_n500_kstar_simulate12750000_waves${num_waves}_seeds${num_seeds}_num6700000 dzA_values_n500_kstar_simulate12750000_waves${num_waves}_seeds${num_seeds}_num6700000 | tee ${ESTIMATION_OUT}
 
+../.././scripts/EEEstimation2textableSingleModel.sh -t ${ESTIMATION_OUT}
 ../.././scripts/EEEstimation2textableSingleModel.sh ${ESTIMATION_OUT} > estimated_model.tex
 
 times
