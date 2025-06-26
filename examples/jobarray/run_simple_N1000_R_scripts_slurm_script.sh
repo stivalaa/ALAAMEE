46eabca26e342ec538e1ed205ef5d319fc41654e
Alex Stivala <alex.d.stivala@gmail.com>
2025-06-26 10:42:01 +1000

    formatted plain text estimation output in simple examples

--- examples/jobarray/run_simple_N1000_R_scripts_slurm_script.sh
+++ examples/jobarray/run_simple_N1000_R_scripts_slurm_script.sh
@@ -15,10 +15,14 @@ module load r
 
 uname -a
 
+ESTIM_FILE=estimation_simple_N1000.txt
+
 time Rscript ../../R/plotALAAMEEResults.R theta_values_n1000_kstar_simulate12750000 dzA_values_n1000_kstar_simulate12750000
 
 
-time Rscript ../../R/computeALAMEEcovariance.R theta_values_n1000_kstar_simulate12750000 dzA_values_n1000_kstar_simulate12750000
+time Rscript ../../R/computeALAMEEcovariance.R theta_values_n1000_kstar_simulate12750000 dzA_values_n1000_kstar_simulate12750000 | tee ${ESTIM_FILE}
+
+../../scripts/EEEstimation2textableSingleModel.sh -t ${ESTIM_FILE}
 
 times
 echo -n "ended at: "; date
--- examples/jobarray/run_simple_R_scripts_slurm_script.sh
+++ examples/jobarray/run_simple_R_scripts_slurm_script.sh
@@ -14,10 +14,14 @@ module load r
 
 uname -a
 
+ESTIM_FILE=estimation_simple.txt
+
 time Rscript ../..//R/plotALAAMEEResults.R theta_values_n500_kstar_simulate12750000 dzA_values_n500_kstar_simulate12750000
 
 
-time Rscript ../..//R/computeALAMEEcovariance.R theta_values_n500_kstar_simulate12750000 dzA_values_n500_kstar_simulate12750000
+time Rscript ../..//R/computeALAMEEcovariance.R theta_values_n500_kstar_simulate12750000 dzA_values_n500_kstar_simulate12750000 | tee ${ESTIM_FILE}
+
+../../scripts/EEEstimation2textableSingleModel.sh -t ${ESTIM_FILE}
 
 times
 echo -n "ended at: "; date
