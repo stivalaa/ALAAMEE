6464843e1a9b26310fdeec7d74de65d4e938fe79
Alex Stivala <alex.d.stivala@gmail.com>
2025-06-26 11:51:33 +1000

    add new scripts to all parallel/ examples

--- examples/parallel/directed/run_simple_example_directed_parallel_slurm_script.sh
+++ examples/parallel/directed/run_simple_example_directed_parallel_slurm_script.sh
@@ -20,6 +20,8 @@ NUM_RUNS=40
 echo NUM_RUNS = $NUM_RUNS
 NUM_RUNS_MINUS_ONE=`expr ${NUM_RUNS} - 1`
 
+ESTIM_FILE=estimation.txt
+
 # use default (no --jobs option) jobs in parallel (one per CPU default)
 seq 0 ${NUM_RUNS_MINUS_ONE} | parallel --progress --joblog parallel.log ../../../python/runALAAMEESimpleDemoParallelDirected.py
 
@@ -30,7 +32,9 @@ module load r
 Rscript ../../../R/plotALAAMEEResults.R theta_values_highschool_friendship_arclist dzA_values_highschool_friendship_arclist
 
 
-Rscript ../../../R/computeALAMEEcovariance.R theta_values_highschool_friendship_arclist dzA_values_highschool_friendship_arclist
+Rscript ../../../R/computeALAMEEcovariance.R theta_values_highschool_friendship_arclist dzA_values_highschool_friendship_arclist | tee ${ESTIM_FILE}
+
+../../../scripts/EEEstimation2textableSingleModel.sh -t ${ESTIM_FILE}
 
 times
 echo -n "ended at: "; date
